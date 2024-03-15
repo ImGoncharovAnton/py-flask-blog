f70bde783a5dbee3061a4a027861955c5392a91b
Anton Goncharov <gold-library@yandex.ru>
2024-03-15 19:15:28 +0300

    add rus to late translate title for correct urls

--- app/blog/utils.py
+++ app/blog/utils.py
@@ -1,8 +1,21 @@
 from math import ceil
 
+# Translate table
+translit_table = {
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z',
+    'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+    'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch',
+    'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'
+}
+
+
+def transliterate(text):
+    return ''.join(translit_table.get(char, char) for char in text.lower())
+
 
 def slugify(title):
-    return title.lower().replace(' ', '-')
+    transliterated_title = transliterate(title)
+    return transliterated_title.lower().replace(' ', '-')
 
 
 def pagination(count, page, per_page=2):
